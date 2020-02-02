10626b645f2ae22f264d131c6b75bd75236fda8b
George Williamson <keatonwilliamson@Georges-MacBook-Pro.local>
2020-02-02 12:21:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Knob from './Knob.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { value: 0 };
+
+  handleChange = newValue => {
+    this.setState({
+      value: newValue
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Knob
+          size={100}
+          numTicks={1}
+          degrees={260}
+          // min={1}
+          // max={100}
+          value={80}
+          color={true}
+          onChange={this.handleChange}
+        />
+
+        <Knob
+                  size={100}
+          numTicks={1}
+          degrees={180}
+          // min={1}
+          // max={100}
+          value={60}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/Knob.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+class Knob extends React.Component {
+    constructor(props) {
+      super(props);
+
+      // props.degrees is a hard coded value of knobのdegree range. 
+      // start and end angle are read only
+      this.fullAngle = props.degrees;
+      this.startAngle = (360 - props.degrees) / 2;
+      this.endAngle = this.startAngle + props.degrees;
+
+      this.margin = props.size * 0.15;
+
+      // the first two arguments are swapped from the second two arguments when this function is used later in the code... seems like a bug but we'll see
+      this.currentDeg = Math.floor(
+        this.convertRange(
+          // props.min,
+          // props.max,
+          1,
+          10,
+          this.startAngle,
+          this.endAngle,
+          props.value
+        )
+      );
+
+      this.state = { deg: this.currentDeg };
+    }
+  
+    startDrag = e => {
+      e.preventDefault();
+      const knob = e.target.getBoundingClientRect();
+      // points (pts) finds the exact center xy coordinates of your knob 
+      // getBoundingClientRect() left and top finds the distance from the knobのboundaries to the viewportのedges
+      // width/2 and height/2 adds half of the knobs size to that to get to the center
+      const pts = {
+        x: knob.left + knob.width / 2,
+        y: knob.top + knob.height / 2
+      };
+
+      // moveHandler runs continuously on mouse movement
+      const moveHandler = e => {
+
+        console.log("start angle", this.startAngle)
+        console.log("end angle", this.endAngle)
+
+        this.currentDeg = this.getDeg(e.clientX, e.clientY, pts);
+        if (this.currentDeg === this.startAngle) this.currentDeg--;
+        let newValue = Math.floor(
+          this.convertRange(
+            this.startAngle,
+            this.endAngle,
+            this.props.min,
+            this.props.max,
+            this.currentDeg
+          )
+        );
+        this.setState({ deg: this.currentDeg });
+        this.props.onChange(newValue);
+      };
+
+      // onMouseDown from react add listener for movement
+      // clean up on mouseup
+      document.addEventListener("mousemove", moveHandler);
+      document.addEventListener("mouseup", e => {
+        document.removeEventListener("mousemove", moveHandler);
+      });
+    };
+  
+    getDeg = (cX, cY, pts) => {
+      const x = cX - pts.x;
+      const y = cY - pts.y;
+      let deg = Math.atan(y / x) * 180 / Math.PI;
+      if ((x < 0 && y >= 0) || (x < 0 && y < 0)) {
+        deg += 90;
+      } else {
+        deg += 270;
+      }
+      let finalDeg = Math.min(Math.max(this.startAngle, deg), this.endAngle);
+      return finalDeg;
+    };
+  
+    convertRange = (startAngle50, endAngle310, propsMin1, propsMax10, currentDegrees) => {
+      return (currentDegrees - startAngle50) * (propsMax10 - propsMin1) / (endAngle310 - startAngle50) + propsMin1;
+    };
+  
+    dcpy = o => {
+      return JSON.parse(JSON.stringify(o));
+    };
+  
+    render() {
+      let kStyle = {
+        width: this.props.size,
+        height: this.props.size
+      };
+      let iStyle = this.dcpy(kStyle);
+      let oStyle = this.dcpy(kStyle);
+      oStyle.margin = this.margin;
+      if (this.props.color) {
+        oStyle.backgroundImage =
+          "radial-gradient(100% 70%,hsl(210, " +
+          this.currentDeg +
+          "%, " +
+          this.currentDeg / 5 +
+          "%),hsl(" +
+          Math.random() * 100 +
+          ",20%," +
+          this.currentDeg / 36 +
+          "%))";
+      }
+      iStyle.transform = "rotate(" + this.state.deg + "deg)";
+  
+      return (
+        <div className="knob" style={kStyle}>
+          <div className="knob outer" style={oStyle} onMouseDown={this.startDrag}>
+            <div className="knob inner" style={iStyle}>
+              <div className="grip" />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+export default Knob;
